98cb4df2cf053c8163778f2ddf643476037350ee
sjlange <sarahlange21@gmail.com>
2021-08-12 11:20:22 -0700

    update database

--- RVMadeEZ/update_database.py
+++ RVMadeEZ/update_database.py
@@ -8,6 +8,20 @@ print(config['my_dict'])
 print(config['my_dict']['s_index'])
 
 def update_database(file_name):
-    print(pd.read_csv(file_name))
-    my_dict = config['my_dict']
-    print(my_dict)
+    # print(pd.read_csv(file_name))
+
+    input_dict  = config['input_dict']
+    data_dir = config['data_dir']
+
+
+    # df_database = need loop to read file names in our database
+
+    df_input = pd.read_csv(file_name)
+
+    database = glob.glob('../Database/*.csv')
+
+    for i in range(len(database))
+        starname = database[i][:-4]
+        name = np.array(starname)
+        df = pd.DataFrame(data = name)
+        print(df)
