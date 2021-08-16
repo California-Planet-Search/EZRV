3830cf8f7a24c9349f2139f59cd477fa89429e99
sjlange <sarahlange21@gmail.com>
2021-08-16 15:50:05 -0700

    changed name

--- EZRV/__init__.py
+++ EZRV/__init__.py
@@ -1,10 +1,11 @@
 """
-RV_Made_EZ.
+EZRV.
 
 A package that handles literature RV data
 """
 
 from .bc_bjd import *
 from .update_database import *
+from .retrieve_date import *
 __version__ = "0.1.0"
 __author__ = 'Sarah Lange & Fei Dai'
